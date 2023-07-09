8c667d63e76597662327f75ea6b7024b4255ada7
sangharshsparaye <sangharshparaye06@gmail.com>
2023-07-09 15:05:24 +0530

    [sangharsh]This is new Project

---
+++ src/test/java/testSangharsh/DemoSangharsh.java
@@ -0,0 +1,10 @@
+package testSangharsh;
+
+public class DemoSangharsh {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Sangharsh test");
+	}
+
+}
